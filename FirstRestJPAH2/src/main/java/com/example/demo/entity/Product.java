272409b13741fc95912eabe6bbcc55ed24bd8ad8
shifasageer <133022028+shifasageer@users.noreply.github.com>
2024-07-30 14:05:59 +0530

    Add files via upload

---
+++ FirstRestJPAH2/src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Product {
+	
+	@Id
+	private Long productId;
+	private String description;
+	private double price;
+	
+	
+
+}
